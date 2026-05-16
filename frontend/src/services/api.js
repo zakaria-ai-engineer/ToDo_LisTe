797f098d5f62aa3a71a0f1d77eb028030ec7b674
Zakaria Atyq <zakaria.atyq1@gmail.com>
2026-05-16 01:06:16 +0100

    Update

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000'
+const BASE_URL = import.meta.env.VITE_API_URL || 'https://todo-199jiuq4.b4a.run'
 
 const api = axios.create({ baseURL: BASE_URL })
 
